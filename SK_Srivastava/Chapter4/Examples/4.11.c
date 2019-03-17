4f8732968295d71bef90e54426136c004491cdbc
ApoorvaJu08 <apoorva10339@gmail.com>
2019-03-17 21:17:24 +0530

    initiated 4.11

---
+++ SK_Srivastava/Chapter4/Examples/4.11.c
@@ -0,0 +1,5 @@
+// Program for conversion of infix to prefix and evaluation of postfix. It will evaluate only single digit numbers.
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
