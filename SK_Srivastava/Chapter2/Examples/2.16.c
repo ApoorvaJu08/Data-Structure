594631e14ae2e92914bc0061a76adbfe43998083
ApoorvaJu08 <apoorva10339@gmail.com>
2019-04-03 16:46:40 +0530

    2.16

---
+++ SK_Srivastava/Chapter2/Examples/2.16.c
@@ -0,0 +1,16 @@
+// Program to show a function that returns pointer
+#include <stdio.h>
+int *fun(int *p, int n);
+int main()
+{
+    int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, n, *ptr;
+    n = 5;
+    ptr = fun(arr, n);
+    printf("arr = %p, ptr = %p, *Ptr = %d\n", arr, ptr, *ptr);
+}
+
+int *fun(int *p, int n)
+{
+    p = p+n;
+    return p;
+}
