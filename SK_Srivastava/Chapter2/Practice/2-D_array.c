040fcce03c1eef9dc0cddd15b9b907170980831b
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-08 16:34:56 +0530

    2d array

---
+++ SK_Srivastava/Chapter2/Practice/2-D_array.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int mat[][3] = { {1, 10}, {2, 20, 200}, {3}, {4, 40, 400} };
+}
+//Now the number of rows cannot be increased since after initialization compilor knows the row size
