54c795cb5aa88a0cd6ccfa95d92089e4d77875ae
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-12 00:56:16 +0530

    create list function

--- SK_Srivastava/Chapter3/Examples/3.1.c
+++ SK_Srivastava/Chapter3/Examples/3.1.c
@@ -242,3 +242,22 @@ struct node *addatpos(struct node *start, int data, int pos)
     }
     return start;
 }
+struct node *create_list(struct node *start)
+{
+    int i, n, data;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    start = NULL;
+    if(n == 0)
+        return start;
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &data);
+    start = addatbeg(start, data);
+    for(i = 2; i <= n; i++)
+    {
+        printf("Enter the element to be inserted: ");
+        scanf("%d", &data);
+        start = addatend(start, data);
+    }
+    return start;
+}
