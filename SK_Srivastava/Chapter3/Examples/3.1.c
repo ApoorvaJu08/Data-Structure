59a26ef29edc165c1d2055e4c08c571dbae62ff1
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-12 01:06:32 +0530

    delete function

--- SK_Srivastava/Chapter3/Examples/3.1.c
+++ SK_Srivastava/Chapter3/Examples/3.1.c
@@ -261,3 +261,33 @@ struct node *create_list(struct node *start)
     }
     return start;
 }
+struct node *del(struct node *start, int data)
+{
+    struct node *tmp, *p;
+    if(start == NULL)
+    {
+        printf("List is empty\n");
+        return start;
+    }
+    if(start->info == data) //Deletion of first node
+    {
+        tmp = start;
+        start = start -> link;
+        free(tmp);
+        return start;
+    }
+    p = start; //Deletion in between or at the end
+    while(p -> link != NULL)
+    {
+        if(p -> link -> info == data)
+        {
+            tmp = p -> link;
+            p -> link = tmp -> link;
+            free(tmp);
+            return start;
+        }
+        p = p -> link;
+    }
+    printf("Element %d not found\n", data);
+    return start;
+}
