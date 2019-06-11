896c7b47c9a786a10e6f90825bfa639ad1307864
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-12 00:28:08 +0530

    addbefore function

--- SK_Srivastava/Chapter3/Examples/3.1.c
+++ SK_Srivastava/Chapter3/Examples/3.1.c
@@ -183,3 +183,36 @@ struct node *addafter(struct node *start, int data, int item)
     printf("%d not present in the list\n", item);
     return start;
 }
+struct node *addbefore(struct node *start, int data, int item)
+{
+    struct node *tmp, *p;
+    if(start == NULL)
+    {
+        printf("List is empty\n");
+        return start;
+    }
+    //If data is to be inserted before first node
+    if(item == start -> info)
+    {
+        tmp = (struct node *)malloc(sizeof(struct node));
+        tmp -> info = data;
+        tmp -> link = start;
+        start = tmp;
+        return start;
+    }
+    p = start;
+    while(p -> link != NULL)
+    {
+        if(p -> link -> info == item)
+        {
+            tmp = (struct node *)malloc(sizeof(struct node));
+            tmp -> info = data;
+            tmp -> link = p -> link;
+            p -> link = tmp;
+            return start;
+        }
+        p = p -> link;
+    }
+    printf("%d not present in the list\n", item);
+    return start;
+}
