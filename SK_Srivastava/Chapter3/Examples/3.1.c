50c4eb07f1e9cfcf21e64a646a3df3355bd2dbca
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-12 00:11:25 +0530

    addafter function

--- SK_Srivastava/Chapter3/Examples/3.1.c
+++ SK_Srivastava/Chapter3/Examples/3.1.c
@@ -164,3 +164,22 @@ struct node *addatend(struct node *start, int data)
     tmp -> link = NULL;
     return start;
 }
+struct node *addafter(struct node *start, int data, int item)
+{
+    struct node *tmp, *p;
+    p = start;
+    while(p != NULL)
+    {
+        if(p -> info == item)
+        {
+            tmp = (struct node *)malloc(sizeof(struct node));
+            tmp -> info = data;
+            tmp -> link = p -> link;
+            p -> link = tmp;
+            return start;
+        }
+        p =  p -> link;
+    }
+    printf("%d not present in the list\n", item);
+    return start;
+}
