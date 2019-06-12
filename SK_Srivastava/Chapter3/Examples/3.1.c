ffff6253053ebc999b50f80bc3085a9e39183e9d
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-12 23:16:32 +0530

    reverse function

--- SK_Srivastava/Chapter3/Examples/3.1.c
+++ SK_Srivastava/Chapter3/Examples/3.1.c
@@ -291,3 +291,18 @@ struct node *del(struct node *start, int data)
     printf("Element %d not found\n", data);
     return start;
 }
+struct node *reverse(struct node *start)
+{
+    struct node *prev, *ptr, *next;
+    prev = NULL;
+    ptr = start;
+    while(ptr != NULL)
+    {
+        next = ptr -> link;
+        ptr -> link = prev;
+        prev = ptr;
+        ptr = next;
+    }
+    start = prev;
+    return start;
+}
