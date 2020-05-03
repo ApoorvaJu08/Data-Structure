d01c17c0cb5de473c2a9b3d32bf0bfbc8396ef44
ApoorvaJu08 <apoorva10339@gmail.com>
2020-05-03 18:06:07 +0530

    recursive functions for traversals

--- SK_Srivastava/Chapter6/Examples/binary_tree.c
+++ SK_Srivastava/Chapter6/Examples/binary_tree.c
@@ -1,92 +1,65 @@
 #include <stdio.h>
-#define MAX 50
+#include <stdlib.h>
 struct node {
     struct node *lchild;
-    char data;
+    int info;
     struct node *rchild;
 };
-
-/*recursive functions for traversal */
-void preorder(struct node *ptr)
-{
-    if(ptr == NULL)
+void preorder(struct node *ptr);
+void inorder(struct node *ptr);
+void postorder(struct node *ptr);
+main(){
+    struct node *root=NULL, *ptr;
+    int choice, k;
+    while(1){
+        printf("\n");
+        printf("1.Recursive Preorder Traversal\n");
+        printf("2.Recursive Inorder Traversal\n");
+        printf("3.Recursive Postorder Traversal\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            preorder(root);
+            break;
+        case 2:
+            inorder(root);
+            break;
+        case 3:
+            postorder(root);
+            break;
+        case 4:
+            exit(1);
+        default:
+            printf("Wrong choice\n");
+        }
+    }
+}
+void preorder(struct node *ptr){
+    if(ptr == NULL) /*Base case*/
+    {   printf("Tree is empty\n");
         return;
-    printf("%d ", ptr->data);
+    }
+    printf("%d", ptr->info);
     preorder(ptr->lchild);
     preorder(ptr->rchild);
 }
-
-void inorder(struct node *ptr)
-{
-    if(ptr == NULL)
+void inorder(struct node *ptr){
+    if(ptr == NULL) /*Base case*/
+    {   printf("Tree is empty\n");
         return;
+    }
     inorder(ptr->lchild);
-    printf("%d ", ptr->data);
+    printf("%d", ptr->info);
     inorder(ptr->rchild);
 }
-
-void postorder(struct node *ptr)
-{
-    if(ptr == NULL)
+void postorder(struct node *ptr){
+    if(ptr == NULL) /*Base case*/
+    {   printf("Tree is empty\n");
         return;
+    }
     postorder(ptr->lchild);
     postorder(ptr->rchild);
-    printf("%d ", ptr->data);
+    printf("%d", ptr->info);
 }
-
-/*Non-recursive traversals*/
-struct node *stack[MAX];
-int top = -1;
-void push_stack(struct node *item)
-{
-    if(top == (MAX - 1))
-    {
-        printf("Stack Overflow\n");
-        return;
-    }
-    top = top + 1;
-    stack[top] = item;
-}
-
-struct node *pop_stack()
-{
-    struct node *item;
-    if(top == -1)
-    {
-        printf("Stack Underflow...\n");
-        exit(1);
-    }
-    item = stack[top];
-    top = top - 1;
-    return item;
-}
-
-int stack_empty()
-{
-    if(top == -1)
-        return 1;
-    else
-        return 0;
-}
-
-void nrec_pre(struct node *root)
-{
-    struct node *ptr = root;
-    if(ptr == NULL)
-    {
-        printf("Tree is empty\n");
-        return;
-    }
-    push_stack(ptr);
-    while(!stack_empty())
-    {
-        ptr = pop_stack();
-        printf("%d ", ptr -> data);
-        if(ptr->rchild != NULL)
-            push_stack(ptr -> rchild);
-        if(ptr->lchild != NULL)
-            push_stack(ptr -> lchild);
-    }
-    printf("\n");
-}
-/*non-recursive going on*/
